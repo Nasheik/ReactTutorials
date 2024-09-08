5a49b0ec9a30de7b42b626bd5213adc277daa2f4
Kishan <kshnjshptl@gmail.com>
2024-09-07 20:40:47 -0400

    [Added] - Database Connection + Display Blogs

--- modern-blog-app/pages/index.js
+++ modern-blog-app/pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head"
 import { PostCard,Categories,PostWidget } from "../components";
+import {getPosts} from "../services"
 
-const posts = [
-  {title: "React Testing", excerpt: "Learn React Testing"},
-  {title: "React With Tailwind", excerpt: "Learn React Testing with Tailwind"},
-];
 
-export default function Home() {
+
+export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -15,7 +13,7 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post,index) => <PostCard post={post} key={post.title}/>)}
+          {posts.map((post,index) => <PostCard post={post.node} key={post.title}/>)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -26,4 +24,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+  console.log(posts)
+  return { 
+    props: {posts}
+  }
 }

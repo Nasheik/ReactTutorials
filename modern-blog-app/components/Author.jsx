c37bb4356d364cb4d7d2abe8b152e8f69a4f13d2
Kishan <kshnjshptl@gmail.com>
2024-09-08 16:45:49 -0400

    [Ongoing] - Post Details Page

--- modern-blog-app/components/Author.jsx
+++ modern-blog-app/components/Author.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div className='items-center text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+        <div className='absolute'>
+            <img
+                alt={author.name}
+                height="100px"
+                width="100px"
+                className='align-bottom rounded-full'
+                src={author.photo.url}
+            />
+        </div>
+        <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+        <p className='text-white text-lg'> {author.bio} </p>
+    </div>
   )
 }
 

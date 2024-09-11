24ad120e947d4b31434ce2eb28cdae6ed2c06d65
Kishan <kshnjshptl@gmail.com>
2024-09-11 12:11:13 -0400

    [Added] - Category Pages [Deployed] - To Vercel

--- modern-blog-app/services/index.js
+++ modern-blog-app/services/index.js
@@ -181,3 +181,82 @@ export const getFeaturedPosts = async () => {
   
     return result.posts;
   };
+
+  export const getAdjacentPosts = async (createdAt, slug) => {
+    const query = gql`
+      query GetAdjacentPosts($createdAt: DateTime!,$slug:String!) {
+        next:posts(
+          first: 1
+          orderBy: createdAt_ASC
+          where: {slug_not: $slug, AND: {createdAt_gte: $createdAt}}
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+        previous:posts(
+          first: 1
+          orderBy: createdAt_DESC
+          where: {slug_not: $slug, AND: {createdAt_lte: $createdAt}}
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug, createdAt });
+  
+    return { next: result.next[0], previous: result.previous[0] };
+  };
+
+
+`        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: {slug_not: $slug, AND: {category_some: {slug_in: $categories}}}
+                last: 3
+            ){`
+  
+  export const getCategoryPost = async (slug) => {
+    const query = gql`
+      query GetCategoryPost($slug: String!) {
+        postsConnection(where: {category_some: {slug: $slug}}) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              category {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.postsConnection.edges;
+  };

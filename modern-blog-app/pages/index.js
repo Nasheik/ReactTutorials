e3ed2a334a76fbc4e265b49fdc2388b238b32fb9
Kishan <kshnjshptl@gmail.com>
2024-09-11 11:29:49 -0400

    [Added] - Featured Posts

--- modern-blog-app/pages/index.js
+++ modern-blog-app/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head"
-import { PostCard,Categories,PostWidget } from "../components";
+import { PostCard,Categories,PostWidget,FeaturedPostCard } from "../components";
 import {getPosts} from "../services"
+import {FeaturedPosts} from "../sections"
+
 
 
 
@@ -11,6 +13,7 @@ export default function Home({posts}) {
         <title>Blogging App</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
+      <FeaturedPosts/>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post,index) => <PostCard post={post.node} key={post.title}/>)}
